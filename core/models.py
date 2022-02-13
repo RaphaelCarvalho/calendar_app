7b6d8d8c3aae000c7223b03c68e606f676834593
RaphaelCarvalho <p.raphael.carvalho@hotmail.com>
2022-02-13 02:28:25 -0300

    Add Event, Filter and add verbose_name. Alter time_zone also.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Evento')
+    description = models.TextField(blank=True, null=True, verbose_name='Descrição')
+    date_event = models.DateTimeField(verbose_name='Data do evento')
+    date_creation = models.DateTimeField(auto_now=True, verbose_name='Data da Criação')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'db_event'
+
+    def __str__(self):
+        return self.title
