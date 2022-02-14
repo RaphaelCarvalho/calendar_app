586abda1a539ea531caa75e8e2d0be587b9d0c7d
RaphaelCarvalho <p.raphael.carvalho@hotmail.com>
2022-02-13 22:24:14 -0300

    Create View List and template html

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from core.models import Event
+
 
 # Create your views here.
+
+def list_events(request):
+    #user = request.user
+    #event = Event.objects.filter(user=user)
+    event = Event.objects.all()
+    response = {'events': event}
+    return render(request, 'calendar.html', response)
